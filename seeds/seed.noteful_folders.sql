f95db0bffbef280d730d8540547f3ff342da2503
Steven Kang <skang28@gmail.com>
2019-12-14 19:44:34 -0500

    server for noteful app

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_folders (id, name)
+Values
+    (1, 'Important'),
+    (2, 'Super'),
+    (3, 'Spangley')
---
+++ src/notes/notes-router.js
@@ -0,0 +1,104 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const NotesService = require('./Notes-service')
+
+const notesRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeNotes = note => ({
+    id: note.id,
+    name: note.name,
+    date_modified: note.date_modified,
+    folderId: note.folderId,
+    content: note.content
+})
+
+notesRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    NotesService.getAllNotes(knexInstance)
+      .then(notes => {
+        res.json(notes.map(serializeNotes))
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name, date_modified, folderId, content } = req.body
+    const newNote = { name, date_modified, folderId, content }
+
+    for (const [key, value] of Object.entries(newNote))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+    newNote.author = author
+    NotesService.insertNote(
+      req.app.get('db'),
+      newNote
+    )
+      .then(note => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.id}`))
+          .json(serializeNote(note))
+      })
+      .catch(next)
+  })
+
+  notesRouter
+  .route('/:note_id')
+  .all((req, res, next) => {
+    NotesService.getById(
+      req.app.get('db'),
+      req.params.note_id
+    )
+      .then(note => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: `note doesn't exist` }
+          })
+        }
+        res.note = note
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeNotes(res.note))
+  })
+  .delete((req, res, next) => {
+    notesService.deleteNote(
+      req.app.get('db'),
+      req.params.note_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { name, date_modified, folderId, content } = req.body
+    const noteToUpdate = { name, date_modified, folderId, content }
+
+    const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'name', 'date_modified', 'folderId', 'content'`
+        }
+      })
+
+    notesService.updateNote(
+      req.app.get('db'),
+      req.params.note_id,
+      noteToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = notesRouter
